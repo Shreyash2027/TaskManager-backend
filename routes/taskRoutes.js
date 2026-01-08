7e691058f5af1f81d75abbc295a1da63085a84b5
Shreyash kumar <shreyashkr0305@gmail.com>
2026-01-09 01:12:30 +0530

    Backend ready for deployment

---
+++ routes/taskRoutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Task = require("../models/Task");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json(task);
+});
+
+
+router.get("/", async (req, res) => {
+  const tasks = await Task.find().sort({ dueDate: 1 });
+  res.json(tasks);
+});
+
+
+router.put("/:id", async (req, res) => {
+  const task = await Task.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(task);
+});
+
+router.delete("/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"));
+
+app.use("/api/tasks", require("./routes/taskRoutes"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
